8262dee7b18f74c9f42db5d1a85dd04cc04ff82e
WojTechM <wojciech.m1997@gmail.com>
2019-05-31 11:41:00 +0200

    Zaimplementowano tworzenie kontekstu na 3 różne sposoby Całość spięta prostym enumem i switchem, aby ułatwić przerzucanie się między trybami podczas prezentacji

--- src/main/java/com/github/wojtechm/Main.java
+++ src/main/java/com/github/wojtechm/Main.java
@@ -7,11 +7,43 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 class Main {
 
+    private enum ŹródłoKonfiguracji {
+        KLASA, PAKIET, REGISTER
+    }
+
+    private static AnnotationConfigApplicationContext context;
+    private static ŹródłoKonfiguracji źródłoKonfiguracji = ŹródłoKonfiguracji.PAKIET;
+
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(HelloWorldConfig.class);
-        context.refresh();
+        stwórzContext();
         HelloWorld helloWorld = context.getBean(HelloWorld.class);
         helloWorld.run();
+    }
+
+    private static void stwórzContext() {
+        switch (źródłoKonfiguracji) {
+            case REGISTER:
+                zarejetrujKlasyZKonfiguracją();
+            case KLASA:
+                wczytajContextZKlasy();
+                break;
+            case PAKIET:
+                wczytajContextZPakietu();
+                break;
+        }
+    }
+
+    private static void zarejetrujKlasyZKonfiguracją() {
+        context = new AnnotationConfigApplicationContext();
+        context.register(HelloWorldConfig.class);
+        context.refresh();
+    }
+
+    private static void wczytajContextZKlasy() {
+        context = new AnnotationConfigApplicationContext(HelloWorldConfig.class);
+    }
+
+    private static void wczytajContextZPakietu() {
+        context = new AnnotationConfigApplicationContext("com.github.wojtechm");
     }
 }
