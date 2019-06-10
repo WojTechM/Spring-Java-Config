714a166f6dbd152656608c2cd30be1722364a968
WojTechM <wojciech.m1997@gmail.com>
2019-06-10 17:15:12 +0200

    Poprawka dokumentacji - zmiana nazw klas

--- src/main/java/com/github/wojtechm/b_wiele_konfiguracji/konfiguracja_źródeł/KonfiguracjaPetardy.java
+++ src/main/java/com/github/wojtechm/b_wiele_konfiguracji/konfiguracja_źródeł/KonfiguracjaPetardy.java
@@ -1,6 +1,6 @@
 package com.github.wojtechm.b_wiele_konfiguracji.konfiguracja_źródeł;
 
-import com.github.wojtechm.b_wiele_konfiguracji.ziarno.MegaWonsz9;
+import com.github.wojtechm.b_wiele_konfiguracji.ziarno.Chomik;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
  * @author Wojciech Makiela
  */
 @Configuration
-public class MegaKonfig {
+public class KonfiguracjaChomika {
 
     @Bean
-    public MegaWonsz9 wonsz9() {
-        return new MegaWonsz9();
+    public Chomik nowyChomik() {
+        return new Chomik();
     }
 }
---
+++ src/main/java/com/github/wojtechm/b_wiele_konfiguracji/ziarno/Petarda.java
@@ -0,0 +1,26 @@
+package com.github.wojtechm.b_wiele_konfiguracji.ziarno;
+
+/**
+ *
+ *             (>\---/<)
+ *             ,'     `.
+ *            /  q   p  \
+ *           (  >(_Y_)<  )
+ *            >-' `-' `-<-.
+ *           /  _.== ,=.,- \
+ *          /,    )`  '(    )
+ *         ; `._.'      `--<
+ *        :     \        |  )
+ *        \      )       ;_/  hjw
+ *         `._ _/_  ___.'-\\\
+ *            `--\\\
+ *
+ *
+ * @author Wojciech Makiela
+ */
+public class Chomik implements Wybuchable {
+    @Override
+    public void wybuchaj() {
+        System.out.println("*Puff*");
+    }
+}
